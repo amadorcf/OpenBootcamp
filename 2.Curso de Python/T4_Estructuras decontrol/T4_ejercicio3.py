b809a8f941d8f46652f492311624ab60ac865647
amadorcf <108862725+amadorcf@users.noreply.github.com>
2022-07-20 11:31:59 +0200

    Números orden inverso
    Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.

---
+++ 2.Curso de Python/T4_Estructuras decontrol/T4_ejercicio3.py
@@ -0,0 +1,13 @@
+# Metodo BUCLE for
+for i in range(100, 0, -1):
+  print(i)
+    
+    
+print("")
+    
+# Metodo reversed()
+numeros = []
+for i in range(1, 101):
+  numeros.append(i)
+for num in reversed(numeros):
+  print(num)
